4cdc2c092c4ce3fd6af44f674747ef54a2baa61c
Robyn Wang <robynwang@Robyns-Multi-Purpose-Macbook-Pro.local>
2021-12-27 14:11:22 -0800

    comments list grouped and renders

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -27,26 +27,25 @@ module Api
  
         visa = HTTParty.get('https://reopen.europa.eu/api/covid/v1/eutcdata/data/en/all/all')
         json_visa = JSON.parse(visa) 
-        all_country_info = json_visa.select { |country| country["nutscode"] == "AUT" }
+        all_country_info = json_visa.select { |country| country["nutscode"] == alpha3 }
         get_domain = all_country_info[0]["indicators"].select {|data| data["comment"] != ""}     
     
-        list = []
-        get_domain.each_with_object({}) do |h| 
-          obj = { }
-          obj["domain_id"] =  h["domain_id"]
-          obj["domain_name"] = h["domain_name"]
-          obj["indicator_id"] = h["indicator_id"]
-          obj["indicator_name"] = h["indicator_name"]
-          obj["comment"] = h["comment"]
+        all_comments_list = []
+        get_domain.each_with_object({}) do |c| 
+          comment = { }
+          comment["domain_id"] =  c["domain_id"]
+          comment["domain_name"] = c["domain_name"]
+          comment["indicator_id"] = c["indicator_id"]
+          comment["indicator_name"] = c["indicator_name"]
+          comment["comment"] = c["comment"]
 
-          list << obj
+          all_comments_list << comment
         end
         
-    
-        puts JSON.pretty_generate(list) 
-        # puts get_domain.select {|info| info["indicator_id"] == 7002}
+        # puts JSON.pretty_generate(new_list) 
+        @sorted_comments_list = all_comments_list.group_by { |d| d["domain_name"] }
 
-        response = {:stats => @country_stats, :travel_advisory => @travel_advisory}
+        response = {:stats => @country_stats, :travel_advisory => @travel_advisory, :comments => @sorted_comments_list}
         render json: response
 
         # country = Country.find_by(slug: params[:slug])
